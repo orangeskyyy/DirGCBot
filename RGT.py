5ddbad7e57ac4d82da69f12b19ae7ae63ca4233b
ntc <nitc0205@qq.com>
2024-04-23 15:56:10 +0800

    pl框架改写

---
+++ RGT.py
@@ -0,0 +1,204 @@
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
+from layer import RGTLayer
+import pytorch_lightning as pl
+from torch import nn
+import torch
+import globals
+import numpy as np
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+
+class RGTDetector(pl.LightningModule):
+    def __init__(self, args,pretrain=False):
+        super(RGTDetector, self).__init__()
+        self.lr = args.lr
+        self.l2_reg = args.l2_reg
+        self.device_name = args.device_name
+        self.pretrain = pretrain
+        self.batch_size = args.batch_size
+        self.test_batch_size = args.test_batch_size
+        self.linear_channels = args.linear_channels
+        self.user_numeric_num = args.user_numeric_num
+        self.user_cat_num = args.user_cat_num
+        self.user_des_channel = args.user_des_channel
+        self.user_tweet_channel = args.user_tweet_channel
+
+        self.user_in_linear_numeric = nn.Linear(args.user_numeric_num, int(args.linear_channels / 4), bias=True)
+        self.user_in_linear_bool = nn.Linear(args.user_cat_num, int(args.linear_channels / 4), bias=True)
+        self.user_in_linear_des = nn.Linear(args.user_des_channel, int(args.linear_channels / 4), bias=True)
+        self.user_in_linear_tweet = nn.Linear(args.user_tweet_channel, int(args.linear_channels / 4), bias=True)
+        self.user_linear = nn.Linear(args.linear_channels, args.linear_channels)
+
+        self.RGT_layer1 = RGTLayer(num_edge_type=args.edge_type, in_channels=args.linear_channels,
+                                   out_channels=args.out_channels,
+                                   trans_head=args.trans_head, semantic_head=args.semantic_head, dropout=args.dropout)
+        self.RGT_layer2 = RGTLayer(num_edge_type=args.edge_type, in_channels=args.linear_channels,
+                                   out_channels=args.out_channels,
+                                   trans_head=args.trans_head, semantic_head=args.semantic_head, dropout=args.dropout)
+
+        self.out = torch.nn.Linear(args.out_channels, args.user_channel)
+        self.classifier = torch.nn.Linear(args.user_channel, 2)
+
+        self.drop = nn.Dropout(args.dropout)
+        self.CELoss = nn.CrossEntropyLoss()
+        self.ReLU = nn.LeakyReLU()
+
+        self.init_weight()
+
+    def init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                torch.nn.init.kaiming_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.fill_(0.0)
+
+    def training_step(self, train_batch, batch_idx):
+        cat_features = train_batch.x[:, :self.user_cat_num]
+        prop_features = train_batch.x[:, self.user_cat_num: self.user_cat_num + self.user_numeric_num]
+        tweet_features = train_batch.x[:,
+                         self.user_cat_num + self.user_numeric_num: self.user_cat_num + self.user_numeric_num + self.user_tweet_channel]
+        des_features = train_batch.x[:,
+                       self.user_cat_num + self.user_numeric_num + self.user_tweet_channel: self.user_cat_num + self.user_numeric_num + self.user_tweet_channel + self.user_des_channel]
+        label = train_batch.y
+
+        edge_index = train_batch.edge_index
+        edge_type = train_batch.edge_attr.view(-1)
+
+        user_features_numeric = self.drop(self.ReLU(self.user_in_linear_numeric(prop_features)))
+        user_features_bool = self.drop(self.ReLU(self.user_in_linear_bool(cat_features)))
+        user_features_tweet = self.drop(self.ReLU(self.user_in_linear_tweet(tweet_features)))
+        user_features_des = self.drop(self.ReLU(self.user_in_linear_des(des_features)))
+
+        user_features = torch.cat((user_features_numeric, user_features_bool, user_features_tweet, user_features_des),
+                                  dim=1)
+        user_features = self.drop(self.ReLU(self.user_linear(user_features)))
+
+        user_features = self.ReLU(self.RGT_layer1(user_features, edge_index, edge_type))
+        user_features = self.ReLU(self.RGT_layer2(user_features, edge_index, edge_type))
+        if self.pretrain:
+            pass
+        else:
+
+            user_features = self.drop(self.ReLU(self.out(user_features)))
+
+            pred = self.classifier(user_features)
+            loss = self.CELoss(pred, label)
+            return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        self.eval()
+        with torch.no_grad():
+
+            cat_features = val_batch.x[:, :self.user_cat_num]
+            prop_features = val_batch.x[:, self.user_cat_num: self.user_cat_num + self.user_numeric_num]
+            tweet_features = val_batch.x[:,
+                             self.user_cat_num + self.user_numeric_num: self.user_cat_num + self.user_numeric_num + self.user_tweet_channel]
+            des_features =val_batch.x[:,
+                           self.user_cat_num + self.user_numeric_num + self.user_tweet_channel: self.user_cat_num + self.user_numeric_num + self.user_tweet_channel + self.user_des_channel]
+
+
+            edge_index = val_batch.edge_index
+            edge_type = val_batch.edge_attr.view(-1)
+
+            user_features_numeric = self.drop(self.ReLU(self.user_in_linear_numeric(prop_features)))
+            user_features_bool = self.drop(self.ReLU(self.user_in_linear_bool(cat_features)))
+            user_features_tweet = self.drop(self.ReLU(self.user_in_linear_tweet(tweet_features)))
+            user_features_des = self.drop(self.ReLU(self.user_in_linear_des(des_features)))
+
+            user_features = torch.cat(
+                (user_features_numeric, user_features_bool, user_features_tweet, user_features_des),
+                dim=1)
+            user_features = self.drop(self.ReLU(self.user_linear(user_features)))
+
+
+            user_features = self.ReLU(self.RGT_layer1(user_features, edge_index, edge_type))
+            user_features = self.ReLU(self.RGT_layer2(user_features, edge_index, edge_type))
+
+            user_features = self.drop(self.ReLU(self.out(user_features)))
+            pred = self.classifier(user_features)
+            pred_binary = torch.argmax(pred, dim=1)
+
+            label = val_batch.y
+            acc = accuracy_score(label.cpu(), pred_binary.cpu())
+            f1 = f1_score(label.cpu(), pred_binary.cpu())
+
+            self.log("val_acc", acc)
+            self.log("val_f1", f1)
+
+            print("acc: {} f1: {}".format(acc, f1))
+            # mask = torch.tensor(val_batch.n_id.clone().detach(),dtype=int,device=self.device_name)
+            # label = label[mask]
+            # # print(pred.size())
+            # # todo rename
+            globals.fine_pred_val += list(pred_binary.squeeze().cpu())
+
+            globals.fine_label_val += list(label.squeeze().cpu())
+
+    def on_validation_epoch_end(self):
+        # precision = precision_score(globals.fine_label_val, globals.fine_pred_val,average="macro",zero_division=0)
+        # recall = recall_score(globals.fine_label_val, globals.fine_pred_val,average="macro",zero_division=0)
+        # f1 = f1_score(globals.fine_label_val, globals.fine_pred_val, average="macro", zero_division=0)
+        f1 = f1_score(globals.fine_label_val, globals.fine_pred_val)
+        acc = accuracy_score(globals.fine_label_val, globals.fine_pred_val)
+        globals.fine_label_val = []
+        globals.fine_pred_val = []
+
+        self.log('val_acc', acc, prog_bar=True)
+        self.log('val_f1', f1, prog_bar=True)
+        # self.validation_step_outputs.clear()
+
+    def test_step(self, test_batch, batch_id):
+        self.eval()
+        with torch.no_grad():
+            cat_features = test_batch.x[:, :self.user_cat_num]
+            prop_features = test_batch.x[:, self.user_cat_num: self.user_cat_num + self.user_numeric_num]
+            tweet_features = test_batch.x[:,
+                             self.user_cat_num + self.user_numeric_num: self.user_cat_num + self.user_numeric_num + self.user_tweet_channel]
+            des_features = test_batch.x[:,
+                           self.user_cat_num + self.user_numeric_num + self.user_tweet_channel: self.user_cat_num + self.user_numeric_num + self.user_tweet_channel + self.user_des_channel]
+            label = test_batch.y
+
+            edge_index = test_batch.edge_index
+            edge_type = test_batch.edge_attr.view(-1)
+
+            user_features_numeric = self.drop(self.ReLU(self.user_in_linear_numeric(prop_features)))
+            user_features_bool = self.drop(self.ReLU(self.user_in_linear_bool(cat_features)))
+            user_features_tweet = self.drop(self.ReLU(self.user_in_linear_tweet(tweet_features)))
+            user_features_des = self.drop(self.ReLU(self.user_in_linear_des(des_features)))
+
+            user_features = torch.cat(
+                (user_features_numeric, user_features_bool, user_features_tweet, user_features_des),
+                dim=1)
+            user_features = self.drop(self.ReLU(self.user_linear(user_features)))
+
+            user_features = self.ReLU(self.RGT_layer1(user_features, edge_index, edge_type))
+            user_features = self.ReLU(self.RGT_layer2(user_features, edge_index, edge_type))
+
+            user_features = self.drop(self.ReLU(self.out(user_features)))
+            pred = self.classifier(user_features)
+            # print(pred.size())
+            pred_binary = torch.argmax(pred, dim=1)
+
+            acc = accuracy_score(label.cpu(), pred_binary.cpu())
+            f1 = f1_score(label.cpu(), pred_binary.cpu())
+            precision = precision_score(label.cpu(), pred_binary.cpu())
+            recall = recall_score(label.cpu(), pred_binary.cpu())
+            # auc = roc_auc_score(label.cpu(), pred[:, 1].cpu())
+
+            self.log("acc", acc)
+            self.log("f1", f1)
+            self.log("precision", precision)
+            self.log("recall", recall)
+            # self.log("auc", auc)
+
+            print("acc: {} \t f1: {} \t precision: {} \t recall: {}".format(acc, f1, precision, recall))
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=self.l2_reg, amsgrad=False)
+        scheduler = CosineAnnealingLR(optimizer, T_max=16, eta_min=0)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler
+            },
+        }
---
+++ main.py
@@ -0,0 +1,112 @@
+import argparse
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+import torch
+import torch_geometric
+from torch_geometric.data import Data
+from torch_geometric.loader import NeighborLoader,DataLoader
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, balanced_accuracy_score, \
+    roc_auc_score, mean_squared_error
+from os import listdir
+import globals
+from Dataset import BotDataset
+from RGT import RGTDetector
+import numpy as np
+import random
+import os
+import json
+
+
+def build_args():
+    parser = argparse.ArgumentParser(
+        description="Reproduction of Heterogeneity-aware Bot detection with Relational Graph Transformers")
+    parser.add_argument("--path", type=str, default="E:/论文/datasets/processed_data/TwiBot-20/", help="dataset path Cresci-15 TwiBot-20")
+    parser.add_argument("--linear_channels", type=int, default=128, help="linear channel")
+    parser.add_argument("--user_channel", type=int, default=64, help="user channel")
+    parser.add_argument("--user_numeric_num", type=int, default=5, help="numerical feature channel")
+    parser.add_argument("--user_cat_num", type=int, default=3, help="TwiBot=3 ,Cresci-15=1 categorical features channel")
+    parser.add_argument("--user_des_channel", type=int, default=768, help="description channel")
+    parser.add_argument("--user_tweet_channel", type=int, default=768, help="tweet channel")
+    parser.add_argument("--out_channels", type=int, default=128, help="output channel")
+    parser.add_argument("--dropout", type=float, default=0.5, help="dropout rate")
+    parser.add_argument("--edge_type", type=int, default=2, help="number of edge type")
+    parser.add_argument("--trans_head", type=int, default=2, help="transformer head channel")
+    parser.add_argument("--semantic_head", type=int, default=2, help="description channel")
+    parser.add_argument("--batch_size", type=int, default=128, help="description channel")
+    parser.add_argument("--test_batch_size", type=int, default=128, help="description channel")
+    parser.add_argument("--epochs", type=int, default=200, help="number of epoch")
+    parser.add_argument("--lr", type=float, default=1e-3, help="description channel")
+    parser.add_argument("--l2_reg", type=float, default=3e-5, help="description channel")
+    parser.add_argument("--random_seed", type=int, default=None, help="random")
+    parser.add_argument("--accelerator", type=str, default='cuda')
+    parser.add_argument("--device_name", type=str, default='cuda:0')
+    pretrain = parser.add_mutually_exclusive_group()
+    pretrain.add_argument("--pretrain", action="store_true")
+    pretrain.add_argument("--no-pretrain", action="store_true")
+
+    return parser.parse_args()
+
+
+def load_data(path):
+    print("loading user features...")
+    user_cat_features = torch.load(path + "cat_properties_tensor.pt", map_location='cpu')
+    user_prop_features = torch.load(path + "num_properties_tensor.pt", map_location='cpu')
+    user_tweet_features = torch.load(path + "tweets_tensor.pt", map_location='cpu')
+    user_des_features = torch.load(path + "des_tensor.pt", map_location='cpu')
+    print("user_des_features",user_des_features)
+    # user_x = torch.cat((user_cat_features, user_prop_features, user_tweet_features, user_des_features), dim=1)
+    user_x = torch.cat((user_cat_features, user_prop_features, user_tweet_features, user_des_features), dim=1)[:11826]
+    label = torch.load(path + "label.pt", map_location='cpu')
+    edge_index = torch.load(path + "edge_index.pt", map_location='cpu')
+    edge_type = torch.load(path + "edge_type.pt", map_location='cpu').unsqueeze(-1)
+    data = Data(x=user_x, edge_index=edge_index, edge_attr=edge_type, y=label)
+    data.train_idx = torch.load(path + "train_idx.pt", map_location='cpu')
+    data.valid_idx = torch.load(path + "val_idx.pt", map_location='cpu')
+    data.test_idx = torch.load(path + "test_idx.pt", map_location='cpu')
+    data.n_id = torch.arange(data.num_nodes)
+    return data
+
+
+if __name__ == "__main__":
+    args = build_args()
+
+    acc_list = [];
+    precision_list = [];
+    recall_list = [];
+    f1_list = []
+
+    if args.random_seed != None:
+        pl.seed_everything(args.random_seed)
+
+    # load data
+    dataset = load_data(args.path)
+    print(dataset)
+
+    # set callbacks
+    fine_checkpoint_callback = ModelCheckpoint(
+        monitor='val_acc',
+        mode='max',
+        filename='{epoch:02d}-{val_acc:.4f}',
+        save_top_k=1,
+        verbose=True)
+
+    train_loader = NeighborLoader(dataset, num_neighbors=[32],input_nodes=dataset.train_idx, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    # train_loader = DataLoader(dataset,  batch_size=args.batch_size,shuffle=True, num_workers=2)
+    valid_loader = NeighborLoader(dataset, num_neighbors=[32], input_nodes=dataset.valid_idx, batch_size=args.batch_size, persistent_workers=True,shuffle=True, num_workers=2)
+
+    test_loader = NeighborLoader(dataset, num_neighbors=[32], input_nodes=dataset.test_idx, batch_size=args.test_batch_size, persistent_workers=True, shuffle=True, num_workers=2)
+
+    # valid_dataset = BotDataset(name="valid",path=args.path)
+    # test_dataset = BotDataset(name="test",path=args.path)
+    # valid_loader = DataLoader(valid_dataset, batch_size=1)
+    # test_loader = DataLoader(test_dataset, batch_size=1)
+
+    trainer = pl.Trainer(accelerator=args.accelerator, max_epochs=args.epochs, precision='16-mixed', log_every_n_steps=1, num_nodes=1, callbacks=[fine_checkpoint_callback])
+    model = RGTDetector(args)
+
+    trainer.fit(model, train_loader, valid_loader)
+    dir = './lightning_logs/version_{}/checkpoints/'.format(trainer.logger.version)
+    best_path = './lightning_logs/version_{}/checkpoints/{}'.format(trainer.logger.version, listdir(dir)[0])
+    best_model = RGTDetector.load_from_checkpoint(checkpoint_path=best_path, args=args)
+    trainer.test(best_model, test_loader, verbose=True)
+    # json.dump(vars(args), open('lightning_logs/version_{}/arguments.json'.format(trainer.logger.version), 'w'))
